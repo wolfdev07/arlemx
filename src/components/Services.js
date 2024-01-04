8ee49c9e2397265d4b588212ef60a9d5beebf3f4
Arthur <arthurwolf.dev07@gmail.com>
2024-01-04 16:21:57 -0600

    Update Services

--- src/components/Services.js
+++ src/components/Services.js
@@ -3,6 +3,8 @@ import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 import ImageOne from "../assets/card-service1-min.jpg";
 import ImageTwo from "../assets/card-service2.jpg"
+import ImageThree from "../assets/card-service3.jpg"
+import ImageThreeCard from "../assets/card-service3-min.jpg"
 
 function ModalLess() {
     const [show, setShow] = useState(false);
@@ -13,7 +15,7 @@ function ModalLess() {
     return (
         <>
         <Button className='btn btn-sm' variant="primary" onClick={handleShow}>
-            Ver mas
+            Ver más
         </Button>
 
         <Modal show={show} onHide={handleClose}>
@@ -22,8 +24,17 @@ function ModalLess() {
             </Modal.Header>
             <Modal.Body>
                 <img src={ ImageOne } className='img-thumbnail rounded'/>
-                <p>¿Buscas rentar tu propiedad de manera <strong>Segura</strong> y <strong>Confiable</strong>?, nuestros expertos te ayudaran a crear un Contrato de Arrendamiento sólido.
-                Si eres <strong>Asesor Inmobiliario Profesional</strong>, nuestros servicios te ayudaran a generar confianza en tus clientes, ya que <strong>garantizarás</strong> la protección del patrimonio de tus clientes.</p>
+                <p>¿Buscas rentar tu propiedad de manera <strong>Segura</strong> y <strong>Confiable</strong>?, conoce nuestro servicio:
+                    <ul>
+                        <li><strong>Asesoría Legal Personalizada.</strong></li>
+                        <li>Revisión de Documentos e Integración de <strong>Expediente</strong> del Inquilino.</li>
+                        <li><strong>Investigación</strong> de antecedentes laborales, penales y crediticios del Inquilino.</li>
+                        <li>Elaboración de <strong>Contrato de Arendamiento Integral</strong></li>
+                        <li>Inventario <strong>Fotografico</strong> del inmueble.</li>
+                        <li>Gestión de cobranza electrónica y domiciliaria.</li>
+                        <li>Acompañamiento en todo momento dentro de la duración del arrendamiento.</li>
+                    </ul>
+                </p>
             </Modal.Body>
             <Modal.Footer>
             <Button variant="secondary" onClick={handleClose}>
@@ -48,7 +59,7 @@ function ModalSell() {
     return (
         <>
         <Button className='btn btn-sm' variant="primary" onClick={handleShow}>
-            Ver mas
+            Ver más
         </Button>
 
         <Modal show={show} onHide={handleClose}>
@@ -79,6 +90,39 @@ function ModalSell() {
     );
 }
 
+function ModalReg() {
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
+    return (
+        <>
+        <Button className='btn btn-sm' variant="primary" onClick={handleShow}>
+            Ver más
+        </Button>
+
+        <Modal show={show} onHide={handleClose}>
+            <Modal.Header closeButton>
+            <Modal.Title>Regularización de Inmuebles</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                <img src={ ImageThree } className='img-thumbnail rounded'/>
+                <p>No esperes más, te ayudamos a <strong>Regularizar</strong> tu <strong>Inmueble</strong>, contamos con planes a la medida, <strong>solicita tu primer asesoría gratis.</strong>
+                </p>
+            </Modal.Body>
+            <Modal.Footer>
+            <Button variant="secondary" onClick={handleClose}>
+                Cerrar
+            </Button>
+            <Button variant="primary" onClick={handleClose}>
+                Contactar
+            </Button>
+            </Modal.Footer>
+        </Modal>
+        </>
+    );
+}
 
 
 export function Services() {
@@ -122,13 +166,13 @@ export function Services() {
                 <div className="card mb-3 w-70">
                     <div className="row g-0">
                         <div className="col-4">
-                            <img src={ ImageTwo } className="img-fluid rounded-start" alt="reg"/>
+                            <img src={ ImageThreeCard } className="img-fluid rounded-start" alt="reg"/>
                         </div>
                         <div className="col-8">
                             <div className="card-body">
                                 <h6 className="card-title">Regularización de Inmuebles</h6>
                                 <p className="card-text">Podemos ayudarte a regularizar cualquier propiedad.</p>
-                                
+                                <ModalReg></ModalReg>
                             </div>
                         </div>
                     </div>
